68f6b5226393f7e22db7202e921048cb7c6724e0
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-18 16:25:57 -0500

    cors error and limit access

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/errorHandler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) callback(null, true);
+    else callback(new Error('No permitido'));
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -22,7 +36,6 @@ app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-
 app.listen(port, () => {
-  console.log('Mi port ' +  port);
+  console.log('Mi port ' + port);
 });
