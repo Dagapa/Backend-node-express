1abcc3d3dfce938f656c576a85ed591cc1105285
Dagapa <estebangallego2002@gmail.com>
2023-01-19 20:37:57 -0500

    config db

--- routes/products.router.js
+++ routes/products.router.js
@@ -11,9 +11,13 @@ const {
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await service.find()
+    res.json(products)
+  } catch (error) { 
+    next(error)
+  }
 });
 
 router.get('/filter', (req, res) => {
