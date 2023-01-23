6a945cec85ee345e21018ca44ce14f84b325a98e
Dagapa <estebangallego2002@gmail.com>
2023-01-23 16:00:24 -0500

    debug server errors

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
-const { config } = require('../config/config');
+const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbHost}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const pool = new Pool({ connectionString: URI });
 
